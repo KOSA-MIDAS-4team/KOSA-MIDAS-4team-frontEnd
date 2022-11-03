22f95c421a5f6ec72dbee4b9dc0565755abef62e
soohyang136 <jsh515228@gmail.com>
2022-11-04 01:25:20 +0900

    commit

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,9 +1,11 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios"
+import { format } from "date-fns";
 const Header = () => {
     const [IsCommuted, setIsCommuted] = useState(true);
+    const [todayInfo, setTodayInfo] = useState({});
     const imgSrc = '';
     async function commute() {
         await axios.post("http://13.209.36.143:8081/commute?where=HOME", {}, {headers: {
@@ -29,6 +31,28 @@ const Header = () => {
                 console.log(err);
             })
     }
+    useEffect(() => {
+        const nowDate = new Date();
+        var today = format(nowDate, "d");
+        if (today.length === 1) {
+            today = '0' + today;
+        }
+        const requestDay = `${format(nowDate, "yyyy")}-${format(nowDate, "M")}-${today}`
+        console.log(requestDay);
+        axios.get(`http://13.209.36.143:8081/user/date?date=${requestDay}`, {
+            headers: {
+                Authorization: localStorage.getItem("accessToken"),
+            }
+        })
+        .then(res => {
+            if(res.data.startedAt === res.data.endedAt || res.data.startedAt === undefined){
+                setIsCommuted(false)
+            }
+            else{
+                setIsCommuted(true)
+            }
+        })
+    }, []);
     return (
         <HeaderContainer>
             <Items>
