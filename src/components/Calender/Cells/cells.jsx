9decd5d9c9848ffdb8d25dcff7513ae7a23272e1
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-11-04 02:30:50 +0900

    ADD:: 로그인되지 않았을 시(토큰 만료 포함)  "/login"으로 이동

--- src/components/Calender/Cells/cells.jsx
+++ src/components/Calender/Cells/cells.jsx
@@ -1,103 +1,134 @@
 import { startOfMonth, endOfMonth, startOfWeek, endOfWeek } from 'date-fns';
 import { isSameMonth, isSameDay, addDays, parse, format } from 'date-fns';
 import { useEffect, useState } from 'react';
-import "./cells.style.css";
-import axios from "axios"
+import './cells.style.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Cells = ({ currentMonth, selectedDate, onDateClick }) => {
-    const monthStart = startOfMonth(currentMonth);
-    const monthEnd = endOfMonth(monthStart);
-    const startDate = startOfWeek(monthStart);
-    const endDate = endOfWeek(monthEnd);
+  const monthStart = startOfMonth(currentMonth);
+  const monthEnd = endOfMonth(monthStart);
+  const startDate = startOfWeek(monthStart);
+  const endDate = endOfWeek(monthEnd);
 
-    const rows = [];
-    let days = [];
-    let day = startDate;
-    let formattedDate = '';
-    while (day <= endDate) {
-        for (let i = 0; i < 7; i++) {
-            formattedDate = format(day, 'd');
-            const cloneDay = day;
+  const rows = [];
+  let days = [];
+  let day = startDate;
+  let formattedDate = '';
+  while (day <= endDate) {
+    for (let i = 0; i < 7; i++) {
+      formattedDate = format(day, 'd');
+      const cloneDay = day;
 
-            // console.log(`${format(day, "yyyy")}-${format(day, "M")}-${formattedDate}`)
-            days.push(
-                <Day key={day} day={day} currentMonth={currentMonth} selectedDate={selectedDate} cloneDay={cloneDay} formattedDate={formattedDate} monthStart={monthStart}></Day>
-            );
-            day = addDays(day, 1);
-        }
-        rows.push(
-            <div className="row" key={day}>
-                {days}
-            </div>,
-        );
-        days = [];
+      // console.log(`${format(day, "yyyy")}-${format(day, "M")}-${formattedDate}`)
+      days.push(
+        <Day
+          key={day}
+          day={day}
+          currentMonth={currentMonth}
+          selectedDate={selectedDate}
+          cloneDay={cloneDay}
+          formattedDate={formattedDate}
+          monthStart={monthStart}
+        ></Day>,
+      );
+      day = addDays(day, 1);
     }
+    rows.push(
+      <div className="row" key={day}>
+        {days}
+      </div>,
+    );
+    days = [];
+  }
 
-    return <div className="body">{rows}</div>;
+  return <div className="body">{rows}</div>;
 };
 
 const Day = (props) => {
-    const [workTime, setWorkTime] = useState({});
-    useEffect(() => {
-        async function draw() {
-            if (isSameMonth(props.day, props.monthStart)) {
-                var today = props.formattedDate
-                if (today.length === 1) {
-                    today = '0' + today;
-                }
-                const requestDay = `${format(props.day, "yyyy")}-${format(props.day, "M")}-${today}`
-                // console.log(requestDay);
-                await axios
-                    .get(`http://13.209.36.143:8081/user/date?date=${requestDay}`, {
-                        headers: {
-                            Authorization: localStorage.getItem("accessToken"),
-                        }
-                    })
-                    .then((res) => {
-                        if(res.data.startedAt !== res.data.endedAt){
-                            console.log(res.data.startedAt + " "+ res.data.endedAt + " " + requestDay);
-                        }
-                        setWorkTime(res.data)
-                    })
+  const navigate = useNavigate();
 
-                    .catch((err) => {
-                        throw new Error(err);
-                    });
-            }
+  const [workTime, setWorkTime] = useState({});
+  useEffect(() => {
+    async function draw() {
+      if (isSameMonth(props.day, props.monthStart)) {
+        var today = props.formattedDate;
+        if (today.length === 1) {
+          today = '0' + today;
         }
-        draw();
-    }, []);
-    // console.log(workTime.startedAt + " asd");
-    return (
-        <div
-            className={`col cell ${!isSameMonth(props.day, props.monthStart)
-                ? 'disabled'
-                : isSameDay(props.day, props.selectedDate)
-                    ? 'selected'
-                    : 'valid'
-                }`}
-            key={props.day}
-        >
-            <span
-                className={
-                    format(props.currentMonth, 'M') !== format(props.day, 'M')
-                        ? 'text not-valid'
-                        : ''
-                }
-            >
-                {props.formattedDate}
+        const requestDay = `${format(props.day, 'yyyy')}-${format(
+          props.day,
+          'M',
+        )}-${today}`;
+        // console.log(requestDay);
+        await axios
+          .get(`http://13.209.36.143:8081/user/date?date=${requestDay}`, {
+            headers: {
+              Authorization: localStorage.getItem('accessToken'),
+            },
+          })
+          .then((res) => {
+            if (res.data.startedAt !== res.data.endedAt) {
+              console.log(
+                res.data.startedAt + ' ' + res.data.endedAt + ' ' + requestDay,
+              );
+            }
+            setWorkTime(res.data);
+          })
 
-            </span>
-            {workTime.startedAt !== undefined ? <><div>
-                {workTime.startedAt !== undefined ? `출근시간 ${workTime.startedAt.substring(11)}` : <></>}
-            </div>
-                
-                    {workTime.endedAt !== undefined && workTime.startedAt !== workTime.endedAt ? <div>퇴근시간 {workTime.endedAt.substring(11)}</div> : <></>}
-                </>
-                : <></>}
+          .catch((err) => {
+            if (err.response.status === 401) {
+              navigate('/login');
+            }
+            throw new Error(err);
+          });
+      }
+    }
+    draw();
+  }, []);
+  // console.log(workTime.startedAt + " asd");
+  return (
+    <div
+      className={`col cell ${
+        !isSameMonth(props.day, props.monthStart)
+          ? 'disabled'
+          : isSameDay(props.day, props.selectedDate)
+          ? 'selected'
+          : 'valid'
+      }`}
+      key={props.day}
+    >
+      <span
+        className={
+          format(props.currentMonth, 'M') !== format(props.day, 'M')
+            ? 'text not-valid'
+            : ''
+        }
+      >
+        {props.formattedDate}
+      </span>
+      {workTime.startedAt !== undefined ? (
+        <>
+          <div>
+            {workTime.startedAt !== undefined ? (
+              `출근시간 ${workTime.startedAt.substring(11)}`
+            ) : (
+              <></>
+            )}
+          </div>
 
-        </div>
-    )
-}
+          {workTime.endedAt !== undefined &&
+          workTime.startedAt !== workTime.endedAt ? (
+            <div>퇴근시간 {workTime.endedAt.substring(11)}</div>
+          ) : (
+            <></>
+          )}
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
 
-export default Cells
+export default Cells;
